23a37c318fcd7241cb571058d7b7433c460c733a
bucky <buckyroberts@gmail.com>
2023-06-14 17:50:09 -0400

    Logging

--- thenewboston/project/settings/templates/settings.dev.py
+++ thenewboston/project/settings/templates/settings.dev.py
@@ -1,2 +1,10 @@
 DEBUG = True
 SECRET_KEY = 'django-insecure--08t722@a3nhs))9%p=fzpda1@3y9c*-kprqzd9@3*w0o18@qe'
+
+LOGGING['formatters']['colored'] = {  # type: ignore
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['thenewboston']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore
