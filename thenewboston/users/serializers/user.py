4250c065d9f180606ad5529a078ff4564f3c1390
bucky <buckyroberts@gmail.com>
2023-06-15 20:18:19 -0400

    Update user creation endpoint

--- thenewboston/users/serializers/user.py
+++ thenewboston/users/serializers/user.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserReadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username')
+
+
+class UserWriteSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(validators=[validate_password], write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('password', 'username')
 
     def create(self, validated_data):
         password = validated_data.pop('password')
--- thenewboston/users/views/user.py
+++ thenewboston/users/views/user.py
@@ -2,19 +2,15 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..serializers.user import UserSerializer
+from ..serializers.user import UserReadSerializer, UserWriteSerializer
 
 
 class UserDetailView(APIView):
 
     @staticmethod
     def post(request):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserWriteSerializer(data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-
-            if user:
-                return Response(status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(UserReadSerializer(user).data, status=status.HTTP_201_CREATED)
