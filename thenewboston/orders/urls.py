81c310fa50a3be858ddd4dd2c3fc57b84646c534
bucky <buckyroberts@gmail.com>
2023-06-21 17:14:15 -0400

    Order endpoints

--- thenewboston/orders/urls.py
+++ thenewboston/orders/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
 from .views.asset_pair import AssetPairViewSet
+from .views.order import OrderViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register('asset_pairs', AssetPairViewSet)
+router.register('orders', OrderViewSet)
 
 urlpatterns = router.urls
