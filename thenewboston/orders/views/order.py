fac36dd76afdd9b78a4dc3966a236b1c713f6989
bucky <buckyroberts@gmail.com>
2023-06-26 16:39:10 -0400

    Deduct order cost from users balance

--- thenewboston/orders/views/order.py
+++ thenewboston/orders/views/order.py
@@ -1,10 +1,13 @@
+from django.db import transaction
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from thenewboston.general.permissions import IsObjectOwnerOrReadOnly
+from thenewboston.wallets.models import Wallet
 
 from ..models import Order
+from ..models.order import OrderType
 from ..order_matching.order_matching_engine import OrderMatchingEngine
 from ..serializers.order import OrderReadSerializer, OrderWriteSerializer
 
@@ -13,10 +16,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsObjectOwnerOrReadOnly]
     queryset = Order.objects.all()
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         order = serializer.save()
+        self.update_wallet_balance(order)
         read_serializer = OrderReadSerializer(order, context={'request': request})
 
         order_matching_engine = OrderMatchingEngine()
@@ -24,12 +29,25 @@ class OrderViewSet(viewsets.ModelViewSet):
 
         return Response(read_serializer.data, status=status.HTTP_201_CREATED)
 
+    @staticmethod
+    def update_wallet_balance(order):
+        if order.order_type == OrderType.BUY:
+            wallet = Wallet.objects.filter(owner=order.owner, core=order.secondary_currency).first()
+            total = order.quantity * order.price
+            wallet.balance -= total
+            wallet.save()
+        elif order.order_type == OrderType.SELL:
+            wallet = Wallet.objects.filter(owner=order.owner, core=order.primary_currency).first()
+            wallet.balance -= order.quantity
+            wallet.save()
+
     def get_serializer_class(self):
         if self.action in ['create', 'partial_update', 'update']:
             return OrderWriteSerializer
 
         return OrderReadSerializer
 
+    @transaction.atomic
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
