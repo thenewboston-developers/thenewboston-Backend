ff0ce05758a3e1fe326448b813cf2155317ce695
bucky <buckyroberts@gmail.com>
2023-07-06 14:43:34 -0400

    Remove default invitation code from model

--- thenewboston/invitations/models/invitation.py
+++ thenewboston/invitations/models/invitation.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.utils.crypto import get_random_string
 
 
 class Invitation(models.Model):
-    code = models.CharField(max_length=6, unique=True, default=get_random_string(6))
+    code = models.CharField(max_length=6, unique=True)
     user = models.OneToOneField('users.User', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
