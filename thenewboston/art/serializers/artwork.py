4f89a41010497193c84a4ae2910766e3448d52c4
bucky <buckyroberts@gmail.com>
2023-09-01 14:17:02 -0400

    Artwork endpoints

---
+++ thenewboston/art/serializers/artwork.py
@@ -0,0 +1,41 @@
+import uuid
+
+import requests
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+
+from thenewboston.users.serializers.user import UserReadSerializer
+
+from ..models import Artwork
+
+
+class ArtworkReadSerializer(serializers.ModelSerializer):
+    creator = UserReadSerializer(read_only=True)
+    owner = UserReadSerializer(read_only=True)
+
+    class Meta:
+        model = Artwork
+        fields = '__all__'
+
+
+class ArtworkWriteSerializer(serializers.ModelSerializer):
+    image_url = serializers.URLField(write_only=True)
+
+    class Meta:
+        model = Artwork
+        fields = ('description', 'image_url', 'name')
+        read_only_fields = (
+            'created_date',
+            'creator',
+            'modified_date',
+            'owner',
+        )
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        image_url = validated_data.pop('image_url')
+        response = requests.get(image_url)
+        image = ContentFile(response.content, f'{uuid.uuid4()}.png')
+        artwork = Artwork.objects.create(creator=request.user, image=image, owner=request.user, **validated_data)
+
+        return artwork
--- thenewboston/art/urls.py
+++ thenewboston/art/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
+from .views.artwork import ArtworkViewSet
 from .views.openai_image import OpenAIImageViewSet
 
 router = SimpleRouter(trailing_slash=False)
+router.register('artworks', ArtworkViewSet)
 router.register('openai_images', OpenAIImageViewSet, basename='openai-images')
 
 urlpatterns = router.urls
