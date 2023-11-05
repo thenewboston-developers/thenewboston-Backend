373b6de3baa58c33185a0e0079a2bc0199f06c62
Bucky Roberts <buckyroberts@gmail.com>
2023-11-05 14:03:58 -0500

    66 - Marketplace updates (#67)

--- thenewboston/art/filters/artwork.py
+++ thenewboston/art/filters/artwork.py
@@ -5,7 +5,9 @@ from ..models import Artwork
 
 class ArtworkFilter(django_filters.FilterSet):
     owner = django_filters.CharFilter(field_name='owner', lookup_expr='exact')
+    price_amount__isnull = django_filters.BooleanFilter(field_name='price_amount', lookup_expr='isnull')
+    price_core__isnull = django_filters.BooleanFilter(field_name='price_core', lookup_expr='isnull')
 
     class Meta:
         model = Artwork
-        fields = ('owner',)
+        fields = ('owner', 'price_amount__isnull', 'price_core__isnull')
