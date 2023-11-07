1cf303e0ada6a04d2774c956da41f29a5020e695
bucky <buckyroberts@gmail.com>
2023-11-06 19:15:56 -0500

    72 - Allow for multiple images to be created at once

--- thenewboston/art/serializers/openai_image.py
+++ thenewboston/art/serializers/openai_image.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 
 class OpenAIImageSerializer(serializers.Serializer):
     description = serializers.CharField(required=True, max_length=1000)
+    quantity = serializers.IntegerField(required=True, min_value=1, max_value=10)
