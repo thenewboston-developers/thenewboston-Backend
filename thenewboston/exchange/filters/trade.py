0a8123f5fc4cc3aed61c37faf9f43dc30593a36a
Bucky Roberts <buckyroberts@gmail.com>
2023-11-12 15:05:33 -0500

    81 - Add filters for trade page (#82)

--- thenewboston/exchange/filters/trade.py
+++ thenewboston/exchange/filters/trade.py
@@ -4,7 +4,14 @@ from ..models import Trade
 
 
 class TradeFilter(django_filters.FilterSet):
+    buy_order__primary_currency__id = django_filters.NumberFilter(field_name='buy_order__primary_currency__id')
+    buy_order__secondary_currency__id = django_filters.NumberFilter(field_name='buy_order__secondary_currency__id')
 
     class Meta:
         model = Trade
-        fields = ('buy_order', 'sell_order')
+        fields = {
+            'buy_order': ['exact'],
+            'sell_order': ['exact'],
+            'buy_order__primary_currency__id': ['exact'],
+            'buy_order__secondary_currency__id': ['exact'],
+        }
