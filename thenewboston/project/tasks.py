04655333fe3a721b905ac6cfe9eb416a8e54c4f0
bucky <buckyroberts@gmail.com>
2024-01-14 15:55:16 -0500

    Trying to have Celery worker stream down socket messages

--- thenewboston/project/tasks.py
+++ thenewboston/project/tasks.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from openai import OpenAI
 
+from thenewboston.general.enums import MessageType
+from thenewboston.ia.consumers.message import MessageConsumer
 from thenewboston.ia.models import Message
 from thenewboston.ia.models.message import SenderType
+from thenewboston.ia.serializers.message import MessageReadSerializer
 from thenewboston.ia.utils.ia import get_ia
 
 from .celery import app
@@ -19,7 +22,14 @@ def generate_ias_response(conversation_id):
     response_dict = response.dict()
     text = response_dict['choices'][0]['message']['content']
 
-    Message.objects.create(conversation_id=conversation_id, sender=get_ia(), sender_type=SenderType.IA, text=text)
+    message = Message.objects.create(
+        conversation_id=conversation_id,
+        sender=get_ia(),
+        sender_type=SenderType.IA,
+        text=text,
+    )
+    message_data = MessageReadSerializer(message).data
+    MessageConsumer.stream_message(message_type=MessageType.CREATE_MESSAGE, message_data=message_data)
 
 
 def get_messages(conversation_id):
