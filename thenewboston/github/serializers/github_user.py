98aa26317afc26b2d219d0b3602a97c41bc22613
bucky <buckyroberts@gmail.com>
2024-01-15 16:46:23 -0500

    Add UserReadSerializer to GitHubUserSerializer
    An import statement for UserReadSerializer from the users serializers has been added. This serializer has been used in GitHubUserSerializer to serialize the reward recipient, ensuring we have complete and consistent user data in responses related to GitHub users.

--- thenewboston/github/serializers/github_user.py
+++ thenewboston/github/serializers/github_user.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from thenewboston.users.serializers.user import UserReadSerializer
+
 from ..models import GitHubUser
 
 
 class GitHubUserSerializer(serializers.ModelSerializer):
+    reward_recipient = UserReadSerializer(read_only=True)
 
     class Meta:
         model = GitHubUser
