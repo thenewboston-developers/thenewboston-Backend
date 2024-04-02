adda8e4428bd6357724bee122ebfbe7711c91651
Abdurrehman <abdurrehman.user.95@gmail.com>
2024-04-02 19:38:54 +0500

    unneccessary docstring removed

--- thenewboston/cores/utils/core.py
+++ thenewboston/cores/utils/core.py
@@ -4,7 +4,4 @@ from thenewboston.cores.models import Core
 
 
 def get_default_core():
-    """
-    Returns the default core
-    """
     return Core.objects.get(ticker=settings.DEFAULT_CORE_TICKER)
