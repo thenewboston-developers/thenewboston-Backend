4a3261d96d03aef48b2e1506dbf00079232bc369
Abdurrehman <abdurrehman.user.95@gmail.com>
2024-04-17 13:27:26 +0500

    course serializer in lectures api

--- thenewboston/university/serializers/lecture.py
+++ thenewboston/university/serializers/lecture.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from ..models import Lecture
+from ..serializers.course import CourseReadSerializer
 
 
 class LectureReadSerializer(serializers.ModelSerializer):
+    course = CourseReadSerializer(read_only=True)
 
     class Meta:
         model = Lecture
