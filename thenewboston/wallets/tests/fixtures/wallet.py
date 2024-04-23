22f7f1ebe04938e14a488e7bf0bcf6beea5606b5
bucky <buckyroberts@gmail.com>
2024-04-23 18:43:37 -0400

    Add init file

--- thenewboston/wallets/tests/fixtures/wallet.py
+++ thenewboston/wallets/tests/fixtures/wallet.py
@@ -6,7 +6,7 @@ from model_bakery import baker
 def sample_wallet(db, bucky, sample_core):
     return baker.make(
         'wallets.Wallet',
-        balance=750,
+        balance=1_000,
         core=sample_core,
         owner=bucky,
     )
