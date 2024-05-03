d26120a1de0688ea57f909dc9da3531053dae052
bucky <buckyroberts@gmail.com>
2024-05-03 19:25:05 -0400

    Updates

--- thenewboston/users/serializers/stats.py
+++ thenewboston/users/serializers/stats.py
@@ -7,7 +7,6 @@ User = get_user_model()
 
 
 class StatsSerializer(serializers.ModelSerializer):
-
     default_wallet_balance = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
     following_count = serializers.SerializerMethodField()
@@ -16,22 +15,17 @@ class StatsSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'default_wallet_balance', 'followers_count', 'following_count')
 
-    def __init__(self, *args, **kwargs):
-        super(StatsSerializer, self).__init__(*args, **kwargs)
-
-        # Exclude 'default_wallet_balance' field if the user is not the logged-in user.
-        user = self.context['request'].user
-        if self.instance != user:
-            self.fields.pop('default_wallet_balance', None)
-
-    def get_default_wallet_balance(self, obj):
+    @staticmethod
+    def get_default_wallet_balance(obj):
         wallet = get_default_wallet(obj)
         return wallet.balance if wallet else 0
 
-    def get_followers_count(self, obj):
+    @staticmethod
+    def get_followers_count(obj):
         followers = getattr(obj, 'followers', None)
         return followers.count() if followers else 0
 
-    def get_following_count(self, obj):
+    @staticmethod
+    def get_following_count(obj):
         following = getattr(obj, 'following', None)
         return following.count() if following else 0
