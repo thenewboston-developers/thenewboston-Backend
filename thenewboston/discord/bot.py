e7cf0c018c83a48979713d3a8656a954e53b5707
bucky <buckyroberts@gmail.com>
2024-08-06 20:58:08 -0400

    Updates

--- thenewboston/discord/bot.py
+++ thenewboston/discord/bot.py
@@ -20,6 +20,8 @@ intents.message_content = True
 
 bot = commands.Bot('/', intents=intents)
 
+MESSAGE_HISTORY_LIMIT = 30
+
 
 @bot.event
 async def on_ready():
@@ -27,47 +29,64 @@ async def on_ready():
     logger.info('Discord Logged in as %s (ID: %s)', user, user.id)
 
 
-@bot.command(name='ia')
-async def ia_command(ctx, *, text):
+@bot.event
+async def on_message(message):
+    # TODO(dmu) HIGH: Interact with OpenAI in async way
+
+    if message.author == bot.user:
+        return
+
     try:
-        if not (user := await sync_to_async(get_user_model().objects.get_or_none)(discord_user_id=ctx.author.id)):
-            # TODO(dmu) MEDIUM: Improve reply wording?
-            await ctx.reply('Please, register at https://thenewboston.com')
+        if not (user := await sync_to_async(get_user_model().objects.get_or_none)(discord_user_id=message.author.id)):
+            await message.reply('Please, register at https://thenewboston.com')
             return
 
-        historical_messages = await get_historical_messages(ctx)
+        plain_text_message_history = await get_plain_text_message_history(message.channel)
 
-        # TODO(dmu) HIGH: Interact with OpenAI in async way
-        response = OpenAIClient.get_instance().get_chat_completion(
-            settings.DISCORD_CREATE_RESPONSE_PROMPT_NAME,
-            input_variables={
-                'messages': historical_messages,
-                'text': text
-            },
-            tracked_user=user,
-            tags=['discord_bot_response']
+        answer = OpenAIClient.get_instance().get_chat_completion(
+            settings.DISCORD_IS_RESPONSE_WARRANTED_PROMPT_NAME,
+            input_variables={'plain_text_message_history': plain_text_message_history},
+            tracked_user=user
         )
-        await ctx.reply(response)
+
+        if answer['requiresResponse']:
+            historical_messages = await get_historical_messages(message.channel)
+
+            ias_response = OpenAIClient.get_instance().get_chat_completion(
+                settings.DISCORD_CREATE_RESPONSE_PROMPT_NAME,
+                input_variables={
+                    'messages': historical_messages,
+                    'text': message.content
+                },
+                tracked_user=user,
+                tags=['discord_bot_response']
+            )
+            await message.reply(ias_response)
     except Exception:
-        logger.exception('An error occurred while processing "%s" command', ia_command.name)
-        await ctx.reply('Oops.. Looks like something went wrong. Our team has been notified.')
+        logger.exception('An error occurred while processing the message')
+        await message.reply('Oops.. Looks like something went wrong. Our team has been notified.')
 
 
-async def get_historical_messages(ctx):
+async def get_historical_messages(channel):
     results = []
 
-    async for message in ctx.channel.history(limit=10):
+    async for message in channel.history(limit=MESSAGE_HISTORY_LIMIT):
         if '_ia' in str(message.author):
             results.append({'role': 'assistant', 'content': [{'type': 'text', 'text': message.content}]})
         else:
-            content = message.content
-
-            if content.startswith('/ia'):
-                content = content[3:].strip()
-
-            results.append({'role': 'user', 'content': [{'type': 'text', 'text': content}]})
+            results.append({'role': 'user', 'content': [{'type': 'text', 'text': message.content}]})
 
     return results[::-1]
+
+
+async def get_plain_text_message_history(channel):
+    messages = []
+
+    async for message in channel.history(limit=MESSAGE_HISTORY_LIMIT):
+        author_name = 'ia' if '_ia' in str(message.author) else message.author.name
+        messages.append(f'{author_name}: {message.content}')
+
+    return '\n'.join(messages[::-1])
 
 
 if __name__ == '__main__':
