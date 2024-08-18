ec23c6eadeac8a494729353d58b66c5f2eadba7b
Muhammad Abdurrehman <abdurrehman.user.95@gmail.com>
2024-08-18 17:02:16 +0500

    feat: introduce model for post reaction

---
+++ thenewboston/social/models/post_reaction.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from thenewboston.general.models import CreatedModified
+
+
+class PostReaction(CreatedModified):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.user}-{self.reaction}'
