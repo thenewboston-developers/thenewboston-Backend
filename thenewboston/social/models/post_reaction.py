9e5f1a0f2385d9d0181ce567c1b546dcebb794e0
Muhammad Abdurrehman <abdurrehman.user.95@gmail.com>
2024-08-18 18:43:15 +0500

    feat: add post in PostReaction

--- thenewboston/social/models/post_reaction.py
+++ thenewboston/social/models/post_reaction.py
@@ -5,6 +5,8 @@ from thenewboston.general.models import CreatedModified
 
 class PostReaction(CreatedModified):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post = models.ForeignKey('social.Post', on_delete=models.CASCADE)
+
     reaction = models.CharField(max_length=10)
 
     def __str__(self):
