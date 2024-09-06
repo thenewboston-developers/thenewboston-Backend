c6f25ab1450c4e6e4b469f97bf965f0c99a76e23
Abdurrehman <52558124+MAbdurrehman1@users.noreply.github.com>
2024-09-06 16:42:34 +0500

    feat: post comment notification to owner (#233)

--- thenewboston/social/views/comment.py
+++ thenewboston/social/views/comment.py
@@ -2,7 +2,12 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from thenewboston.general.enums import MessageType
 from thenewboston.general.permissions import IsObjectOwnerOrReadOnly
+from thenewboston.notifications.consumers.notification import NotificationConsumer
+from thenewboston.notifications.models.notification import Notification
+from thenewboston.notifications.serializers.notification import NotificationReadSerializer
+from thenewboston.users.serializers.user import UserReadSerializer
 
 from ..models import Comment
 from ..serializers.comment import CommentReadSerializer, CommentUpdateSerializer, CommentWriteSerializer
@@ -18,7 +23,28 @@ class CommentViewSet(viewsets.ModelViewSet):
         comment = serializer.save()
         read_serializer = CommentReadSerializer(comment, context={'request': request})
 
+        if comment.post.owner != comment.owner:
+            self.notify_post_owner(comment=comment)
+
         return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+    def notify_post_owner(self, comment):
+        post = comment.post
+        notification = Notification.objects.create(
+            owner=post.owner,
+            payload={
+                'post_id': post.id,
+                'commenter': UserReadSerializer(comment.owner).data,
+                'comment': comment.content,
+                'notification_type': 'POST_COMMENT',
+            }
+        )
+
+        notification_data = NotificationReadSerializer(notification).data
+
+        NotificationConsumer.stream_notification(
+            message_type=MessageType.CREATE_NOTIFICATION, notification_data=notification_data
+        )
 
     def get_serializer_class(self):
         if self.action == 'create':
