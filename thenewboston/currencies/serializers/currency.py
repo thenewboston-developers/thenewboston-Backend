9fe733c1eba0484d1fccac94b3a6965f7c415500
Bucky Roberts <buckyroberts@gmail.com>
2025-05-25 16:37:36 -0400

    255 - Require logo when creating currencies (#256)
    * 255 - Require logo when creating currencies
    
    * Cleaning

--- thenewboston/currencies/serializers/currency.py
+++ thenewboston/currencies/serializers/currency.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import exceptions, serializers
 
+from thenewboston.general.utils.image import process_image, validate_image_dimensions
+
 from ..models import Currency
 
 User = get_user_model()
@@ -14,6 +16,7 @@ class CurrencyReadSerializer(serializers.ModelSerializer):
 
 
 class CurrencyWriteSerializer(serializers.ModelSerializer):
+    logo = serializers.ImageField(required=True)
 
     class Meta:
         model = Currency
@@ -31,9 +34,29 @@ class CurrencyWriteSerializer(serializers.ModelSerializer):
             # TODO(dmu) MEDIUM: Use `permission_classes` for checking permissions
             raise exceptions.PermissionDenied('You do not have permission to create a Currency.')
 
+        logo = validated_data.get('logo')
+        if logo:
+            validated_data['logo'] = process_image(logo)
+
         currency = super().create({
             **validated_data,
             'owner': request.user,
         })
 
         return currency
+
+    def update(self, instance, validated_data):
+        logo = validated_data.get('logo')
+        if logo:
+            validated_data['logo'] = process_image(logo)
+
+        return super().update(instance, validated_data)
+
+    def validate_logo(self, value):
+        is_valid, error_message = validate_image_dimensions(value, 512, 512)
+        if not is_valid:
+            raise serializers.ValidationError(error_message)
+
+        # Reset file position to the beginning after validation since Image.open() moves it to the end
+        value.seek(0)
+        return value
