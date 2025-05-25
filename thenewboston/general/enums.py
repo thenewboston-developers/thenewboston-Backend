4d2ece6f3a18a1fbe455ec15898ab05dc604be72
Bucky Roberts <buckyroberts@gmail.com>
2025-05-25 13:20:37 -0400

    247 - Remove CREATE_MESSAGE (#252)

--- thenewboston/general/enums.py
+++ thenewboston/general/enums.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class MessageType(Enum):
     CREATE_EXCHANGE_ORDER = 'create.exchange_order'
-    CREATE_MESSAGE = 'create.message'
     CREATE_NOTIFICATION = 'create.notification'
     UPDATE_EXCHANGE_ORDER = 'update.exchange_order'
     UPDATE_WALLET = 'update.wallet'
