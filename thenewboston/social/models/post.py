4cdb4b6cad58a5b672211b04cf2ca4f0c61ab135
Bucky Roberts <buckyroberts@gmail.com>
2025-05-26 12:34:42 -0400

    259 - Send (#260)
    * 259 - Send
    
    * Cleaning

--- thenewboston/social/models/post.py
+++ thenewboston/social/models/post.py
@@ -7,6 +7,11 @@ class Post(CreatedModified):
     owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True)
+    recipient = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='received_posts', blank=True, null=True
+    )
+    price_amount = models.PositiveBigIntegerField(blank=True, null=True)
+    price_currency = models.ForeignKey('currencies.Currency', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.content[:50]
--- thenewboston/social/serializers/post.py
+++ thenewboston/social/serializers/post.py
@@ -1,8 +1,11 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from thenewboston.general.utils.image import process_image
+from thenewboston.general.utils.transfers import transfer_coins
 from thenewboston.social.serializers.post_reaction import PostReactionsReadSerializer
 from thenewboston.users.serializers.user import UserReadSerializer
+from thenewboston.wallets.models import Wallet
 
 from ..models import Post
 from ..serializers.comment import CommentReadSerializer
@@ -11,14 +14,15 @@ from ..serializers.comment import CommentReadSerializer
 class PostReadSerializer(serializers.ModelSerializer):
     comments = CommentReadSerializer(many=True, read_only=True)
     owner = UserReadSerializer(read_only=True)
+    recipient = UserReadSerializer(read_only=True)
     user_reaction = serializers.SerializerMethodField()
     user_reactions = PostReactionsReadSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
         fields = (
-            'comments', 'content', 'created_date', 'id', 'image', 'modified_date', 'owner', 'user_reaction',
-            'user_reactions'
+            'comments', 'content', 'created_date', 'id', 'image', 'modified_date', 'owner', 'price_amount',
+            'price_currency', 'recipient', 'user_reaction', 'user_reactions'
         )
         read_only_fields = (
             'comments',
@@ -28,35 +32,49 @@ class PostReadSerializer(serializers.ModelSerializer):
             'image',
             'modified_date',
             'owner',
+            'price_amount',
+            'price_currency',
+            'recipient',
         )
 
-    def get_user_reaction(self, obj):
+    @staticmethod
+    def get_user_reaction(obj):
         return getattr(obj, 'user_reaction', None)
 
 
 class PostWriteSerializer(serializers.ModelSerializer):
 
-    is_image_cleared = serializers.BooleanField(
-        default=False,
-        write_only=True,
-        required=False,
-        help_text='A boolean flag indicating whether the existing image should be cleared.'
-    )
-
     class Meta:
         model = Post
-        fields = ('content', 'image', 'is_image_cleared')
+        fields = ('content', 'image', 'recipient', 'price_amount', 'price_currency')
 
+    @transaction.atomic
     def create(self, validated_data):
         request = self.context.get('request')
         image = validated_data.get('image')
-
-        # Removing is_image_cleared from validated_data since it's not part of the Post model
-        validated_data.pop('is_image_cleared', None)
+        recipient = validated_data.get('recipient')
+        price_amount = validated_data.get('price_amount')
+        price_currency = validated_data.get('price_currency')
 
         if image:
             file = process_image(image)
             validated_data['image'] = file
+
+        if price_amount is not None and price_currency is not None and recipient is not None:
+            sender_wallet = Wallet.objects.select_for_update().get(owner=request.user, currency=price_currency)
+
+            if sender_wallet.balance < price_amount:
+                raise serializers.ValidationError('Insufficient funds')
+
+            recipient_wallet, _ = Wallet.objects.select_for_update().get_or_create(
+                owner=recipient, currency=price_currency, defaults={'balance': 0}
+            )
+
+            transfer_coins(
+                sender_wallet=sender_wallet,
+                recipient_wallet=recipient_wallet,
+                amount=price_amount,
+            )
 
         post = super().create({
             **validated_data,
@@ -68,17 +86,60 @@ class PostWriteSerializer(serializers.ModelSerializer):
         """
         Update the Post instance.
 
-        This method updates the content and image of a Post instance. If a new image is provided,
-        it replaces the existing image. If 'is_image_cleared' is True, it removes the current image.
-        Otherwise, it leaves the image unchanged.
+        This method updates the content and image of a Post instance.
+        - If 'image' is not in the request, no changes are made to the image
+        - If a new image is provided, it replaces the existing image
+        - If None/null is provided for image, it clears the image
+
+        Note: Transfer-related fields (recipient, price_amount, price_currency) cannot be updated.
         """
-        image = validated_data.get('image')
+        validated_data.pop('recipient', None)
+        validated_data.pop('price_amount', None)
+        validated_data.pop('price_currency', None)
+
         instance.content = validated_data.get('content', instance.content)
-        if image:
-            file = process_image(image)
-            instance.image = file
-        elif validated_data.get('is_image_cleared'):
-            instance.image = None
+
+        if 'image' in validated_data:
+            image = validated_data.get('image')
+
+            if image is None:
+                instance.image = None
+            else:
+                file = process_image(image)
+                instance.image = file
 
         instance.save()
         return instance
+
+    def validate(self, data):
+        # Only validate transfer fields on create
+        if not self.instance:
+            user = self.context['request'].user
+            recipient = data.get('recipient')
+            price_amount = data.get('price_amount')
+            price_currency = data.get('price_currency')
+
+            fields_provided = [recipient is not None, price_amount is not None, price_currency is not None]
+            if any(fields_provided) and not all(fields_provided):
+                raise serializers.ValidationError(
+                    'If any of recipient, price_amount, or price_currency is provided, all three must be provided.'
+                )
+
+            if recipient and recipient == user:
+                raise serializers.ValidationError('You cannot send coins to yourself.')
+
+            if price_amount is not None and price_currency is not None:
+                try:
+                    Wallet.objects.get(owner=user, currency=price_currency)
+                except Wallet.DoesNotExist:
+                    raise serializers.ValidationError(
+                        f'No wallet found for {price_currency.ticker} for the authenticated user.'
+                    )
+
+        return data
+
+    @staticmethod
+    def validate_price_amount(value):
+        if value is not None and value == 0:
+            raise serializers.ValidationError('price_amount must be greater than 0.')
+        return value
