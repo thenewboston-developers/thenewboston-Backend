687699f5c8e054dfe649b12d6dca74fc93b91e40
bucky <buckyroberts@gmail.com>
2025-06-01 17:46:08 -0400

    Add error handling

--- thenewboston/currencies/views/currency_balance.py
+++ thenewboston/currencies/views/currency_balance.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
 from thenewboston.general.pagination import CustomPageNumberPagination
@@ -16,7 +17,7 @@ class CurrencyBalanceListView(generics.ListAPIView):
         currency_id = self.request.query_params.get('currency__id')
 
         if not currency_id:
-            return Wallet.objects.none()
+            raise ValidationError({'currency__id': 'This query parameter is required.'})
 
         return Wallet.objects.filter(currency_id=currency_id,
                                      balance__gt=0).select_related('owner', 'currency').order_by('-balance')
