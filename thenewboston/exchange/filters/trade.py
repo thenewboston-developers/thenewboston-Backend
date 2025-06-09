c00cee96ae4eaaca522aca3a0181f7a3709d941b
Bucky Roberts <buckyroberts@gmail.com>
2025-06-08 23:57:13 -0400

    298 - Standardize how we pass in IDs as query params (#301)

--- thenewboston/exchange/filters/trade.py
+++ thenewboston/exchange/filters/trade.py
@@ -4,14 +4,14 @@ from ..models import Trade
 
 
 class TradeFilter(django_filters.FilterSet):
-    buy_order__primary_currency__id = django_filters.NumberFilter(field_name='buy_order__primary_currency__id')
-    buy_order__secondary_currency__id = django_filters.NumberFilter(field_name='buy_order__secondary_currency__id')
+    buy_order__primary_currency = django_filters.NumberFilter(field_name='buy_order__primary_currency')
+    buy_order__secondary_currency = django_filters.NumberFilter(field_name='buy_order__secondary_currency')
 
     class Meta:
         model = Trade
         fields = {
             'buy_order': ['exact'],
             'sell_order': ['exact'],
-            'buy_order__primary_currency__id': ['exact'],
-            'buy_order__secondary_currency__id': ['exact'],
+            'buy_order__primary_currency': ['exact'],
+            'buy_order__secondary_currency': ['exact'],
         }
