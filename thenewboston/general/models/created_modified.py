b32df053aae713bfeb650675be87720be7d80666
bucky <buckyroberts@gmail.com>
2025-06-08 23:54:17 -0400

    298 - Standardize how we pass in IDs as query params

--- thenewboston/general/models/created_modified.py
+++ thenewboston/general/models/created_modified.py
@@ -4,9 +4,7 @@ from .custom_model import CustomModel
 
 
 class CreatedModified(CustomModel):
-    # TODO(dmu) LOW: Consider rename: `created_date` -> `created_at`
     created_date = models.DateTimeField(auto_now_add=True)
-    # TODO(dmu) LOW: Consider rename: `modified_date` -> `updated_at`
     modified_date = models.DateTimeField(auto_now=True)
 
     class Meta:
