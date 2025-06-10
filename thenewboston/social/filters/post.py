d966b39068989ba64740c9c5be1cf9be0d39385e
Bucky Roberts <buckyroberts@gmail.com>
2025-06-09 20:02:37 -0400

    291 - Post should show up on sender and recipients profiles (#304)

--- thenewboston/social/filters/post.py
+++ thenewboston/social/filters/post.py
@@ -1,11 +1,17 @@
 import django_filters
+from django.db import models
 
 from ..models import Post
 
 
 class PostFilter(django_filters.FilterSet):
     owner = django_filters.CharFilter(field_name='owner', lookup_expr='exact')
+    user = django_filters.CharFilter(method='filter_user')
 
     class Meta:
         model = Post
-        fields = ('owner',)
+        fields = ('owner', 'user')
+
+    @staticmethod
+    def filter_user(queryset, name, value):
+        return queryset.filter(models.Q(owner=value) | models.Q(recipient=value))
