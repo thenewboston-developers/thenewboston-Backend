cbf0c9e768d8f700a7bd643f51d0aeef15ea5279
bucky <buckyroberts@gmail.com>
2025-06-13 18:20:20 -0400

    Cleaning up code

--- thenewboston/exchange/serializers/trade.py
+++ thenewboston/exchange/serializers/trade.py
@@ -4,17 +4,9 @@ from ..models import Trade
 
 
 class TradeSerializer(serializers.ModelSerializer):
-    primary_currency = serializers.SerializerMethodField()
-    secondary_currency = serializers.SerializerMethodField()
+    primary_currency = serializers.IntegerField(source='buy_order.primary_currency_id', read_only=True)
+    secondary_currency = serializers.IntegerField(source='buy_order.secondary_currency_id', read_only=True)
 
     class Meta:
         model = Trade
         fields = '__all__'
-
-    @staticmethod
-    def get_primary_currency(obj):
-        return obj.buy_order.primary_currency_id
-
-    @staticmethod
-    def get_secondary_currency(obj):
-        return obj.buy_order.secondary_currency_id
