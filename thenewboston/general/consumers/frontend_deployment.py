1f2b2bd1e2b7b353fe0540175994099efe3420f5
bucky <buckyroberts@gmail.com>
2025-06-24 21:02:39 -0400

    Cleaning

--- thenewboston/general/consumers/frontend_deployment.py
+++ thenewboston/general/consumers/frontend_deployment.py
@@ -4,7 +4,6 @@ from channels.layers import get_channel_layer
 
 
 class FrontendDeploymentConsumer(JsonWebsocketConsumer):
-
     GROUP_NAME = 'frontend_deployments'
 
     def connect(self):
@@ -14,12 +13,6 @@ class FrontendDeploymentConsumer(JsonWebsocketConsumer):
     def disconnect(self, close_code):
         async_to_sync(get_channel_layer().group_discard)(self.GROUP_NAME, self.channel_name)
 
-    def update_frontend_deployment(self, event):
-        self.send_json({
-            'frontend_deployment': event['payload'],
-            'type': event['type'],
-        })
-
     @classmethod
     def stream_frontend_deployment(cls, *, message_type, deployment_data):
         channel_layer = get_channel_layer()
@@ -28,3 +21,9 @@ class FrontendDeploymentConsumer(JsonWebsocketConsumer):
             'type': message_type.value,
         }
         async_to_sync(channel_layer.group_send)(cls.GROUP_NAME, deployment_event)
+
+    def update_frontend_deployment(self, event):
+        self.send_json({
+            'frontend_deployment': event['payload'],
+            'type': event['type'],
+        })
