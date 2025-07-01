ce43b62fbb4a61dad5bd6075c5d9cdbe69331b41
bucky <buckyroberts@gmail.com>
2025-06-30 21:49:31 -0400

    342 - Ability to @ mention users

--- thenewboston/users/urls.py
+++ thenewboston/users/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from .views.stats import StatsAPIView
 from .views.user import UserViewSet
+from .views.user_search import user_search
 
 router = SimpleRouter(trailing_slash=False)
 router.register('users', UserViewSet)
@@ -10,4 +11,5 @@ router.register('users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('user/<int:user_id>/stats', StatsAPIView.as_view(), name='user-stats'),
+    path('users/search', user_search, name='user-search'),
 ]
---
+++ thenewboston/users/views/user_search.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from ..models import User
+from ..serializers.user import UserReadSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_search(request):
+    if not (query := request.GET.get('q', '').strip()):
+        return Response({'error': 'Query parameter "q" is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    users = User.objects.filter(username__istartswith=query).order_by('username')[:10]
+    serializer = UserReadSerializer(users, many=True)
+    return Response(serializer.data)
