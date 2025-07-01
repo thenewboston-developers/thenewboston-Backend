78c0f8db9085f07e29e4329989c89bc1c69f30c9
Bucky Roberts <buckyroberts@gmail.com>
2025-06-30 23:20:50 -0400

    342 - Ability to @ mention users (#343)
    * 342 - Ability to @ mention users
    
    * Fixing bug
    
    * Pass in request via context

--- thenewboston/users/urls.py
+++ thenewboston/users/urls.py
@@ -3,11 +3,14 @@ from rest_framework.routers import SimpleRouter
 
 from .views.stats import StatsAPIView
 from .views.user import UserViewSet
+from .views.user_search import user_search
 
 router = SimpleRouter(trailing_slash=False)
 router.register('users', UserViewSet)
 
 urlpatterns = [
+    # Keep users/search before router.urls to prevent it from being matched as users/<pk>
+    path('users/search', user_search, name='user-search'),
     path('', include(router.urls)),
     path('user/<int:user_id>/stats', StatsAPIView.as_view(), name='user-stats'),
 ]
