afa47c3f5be718b2761407bd8d5dcb0e2cd81a7a
Bucky Roberts <buckyroberts@gmail.com>
2025-07-13 13:27:44 -0400

    349 - Currency details page updates (#350)

--- thenewboston/currencies/serializers/currency_balance.py
+++ thenewboston/currencies/serializers/currency_balance.py
@@ -6,7 +6,9 @@ from thenewboston.wallets.models import Wallet
 
 class CurrencyBalanceSerializer(serializers.ModelSerializer):
     owner = UserReadSerializer(read_only=True)
+    rank = serializers.IntegerField(read_only=True)
+    percentage = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Wallet
-        fields = ('owner', 'balance')
+        fields = ('owner', 'balance', 'rank', 'percentage')
