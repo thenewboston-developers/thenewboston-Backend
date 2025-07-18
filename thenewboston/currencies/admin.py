3bd03610b042163a93c6506177747957b63f0ddf
bucky <buckyroberts@gmail.com>
2025-07-18 19:04:53 -0400

    353 - Add whitepaper to Django admin

--- thenewboston/currencies/admin.py
+++ thenewboston/currencies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Currency, Mint
+from .models import Currency, Mint, Whitepaper
 
 admin.site.register(Currency)
 admin.site.register(Mint)
+admin.site.register(Whitepaper)
