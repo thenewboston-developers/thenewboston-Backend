33b29f335de245f9d10c491734415975f6b3eeee
bucky <buckyroberts@gmail.com>
2025-08-02 18:25:31 -0400

    375 - Backend for getAvailableWalletCurrencies

---
+++ thenewboston/currencies/filters/__init__.py
@@ -0,0 +1,15 @@
+from .currency import CurrencyFilter
+from .currency_balance import CurrencyBalanceFilter
+from .mint import MintFilter
+from .mint_chart_data import MintChartDataFilter
+from .total_amount_minted import TotalAmountMintedFilter
+from .whitepaper import WhitepaperFilter
+
+__all__ = [
+    'CurrencyFilter',
+    'CurrencyBalanceFilter',
+    'MintFilter',
+    'MintChartDataFilter',
+    'TotalAmountMintedFilter',
+    'WhitepaperFilter',
+]
---
+++ thenewboston/currencies/filters/currency.py
@@ -0,0 +1,20 @@
+import django_filters
+
+from thenewboston.wallets.models import Wallet
+
+from ..models import Currency
+
+
+class CurrencyFilter(django_filters.FilterSet):
+    no_wallet = django_filters.BooleanFilter(method='filter_no_wallet')
+
+    class Meta:
+        model = Currency
+        fields = ('no_wallet',)
+
+    def filter_no_wallet(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            user_wallet_currencies = Wallet.objects.filter(owner=self.request.user
+                                                           ).values_list('currency_id', flat=True)
+            return queryset.exclude(id__in=user_wallet_currencies)
+        return queryset
