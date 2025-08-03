0f9b912bc903940c58420706c21e2ae56540f883
bucky <buckyroberts@gmail.com>
2025-08-03 01:04:47 -0400

    371 - Endpoint needed to link to "Trade" page from currency details page

--- thenewboston/exchange/views/asset_pair.py
+++ thenewboston/exchange/views/asset_pair.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from ..filters.asset_pair import AssetPairFilter
 from ..models import AssetPair
 from ..serializers.asset_pair import AssetPairSerializer
 
 
 class AssetPairViewSet(viewsets.ReadOnlyModelViewSet):
+    filterset_class = AssetPairFilter
     permission_classes = [IsAuthenticated]
     queryset = AssetPair.objects.all()
     serializer_class = AssetPairSerializer
