1ab80f16cdaea0d2d89760de6d312ac0ae96a358
bucky <buckyroberts@gmail.com>
2025-08-04 20:26:37 -0400

    Final optimization

--- thenewboston/social/views/post.py
+++ thenewboston/social/views/post.py
@@ -38,7 +38,9 @@ class PostViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         if self.action in ['retrieve', 'list']:
-            queryset = queryset.prefetch_related('owner', 'likes')
+            queryset = queryset.prefetch_related(
+                'owner', 'likes', 'comments__owner', 'comments__price_currency', 'price_currency', 'recipient'
+            )
 
         return queryset
 
