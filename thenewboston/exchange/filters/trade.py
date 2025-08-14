6da10a548cad288f8ca80f7683f49f22b8ff2522
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2025-08-14 10:30:17 +0300

    Remove deprecated stuff related to trade currencies

--- thenewboston/exchange/filters/trade.py
+++ thenewboston/exchange/filters/trade.py
@@ -4,21 +4,6 @@ from ..models import Trade
 
 
 class TradeFilter(django_filters.FilterSet):
-    # TODO(dmu) MEDIUM: `buy_order__primary_currency` and `buy_order__secondary_currency`
-    #                   are kept for backward compatibility. Consider removal
-    buy_order__primary_currency = django_filters.NumberFilter(field_name='buy_order__asset_pair__primary_currency')
-    buy_order__secondary_currency = django_filters.NumberFilter(field_name='buy_order__asset_pair__secondary_currency')
-
-    # TODO(dmu) MEDIUM: Consider removal migration of `buy_order__asset_pair__primary_currency` and
-    #                   `buy_order__asset_pair__secondary_currency` to `buy_order__asset_pair`.
-    #                   Otherwise consider introduction of https://pypi.org/project/djangorestframework-filters/
-    buy_order__asset_pair__primary_currency = django_filters.NumberFilter(
-        field_name='buy_order__asset_pair__primary_currency'
-    )
-    buy_order__asset_pair__secondary_currency = django_filters.NumberFilter(
-        field_name='buy_order__asset_pair__secondary_currency'
-    )
-
     buy_order__asset_pair = django_filters.NumberFilter(field_name='buy_order__asset_pair')
 
     class Meta:
@@ -27,7 +12,5 @@ class TradeFilter(django_filters.FilterSet):
             # TODO(dmu) LOW: Are these definitions excessive?
             'buy_order': ['exact'],
             'sell_order': ['exact'],
-            'buy_order__asset_pair__primary_currency': ['exact'],
-            'buy_order__asset_pair__secondary_currency': ['exact'],
             'buy_order__asset_pair': ['exact'],
         }
--- thenewboston/exchange/serializers/trade.py
+++ thenewboston/exchange/serializers/trade.py
@@ -6,10 +6,6 @@ from ..models import Trade
 class TradeSerializer(serializers.ModelSerializer):
     asset_pair = serializers.IntegerField(source='buy_order.asset_pair_id', read_only=True)
 
-    # TODO(dmu) MEDIUM: Consider removal of `primary_currency` and `secondary_currency` in favor of `asset_pair`
-    primary_currency = serializers.IntegerField(source='buy_order.asset_pair.primary_currency_id', read_only=True)
-    secondary_currency = serializers.IntegerField(source='buy_order.asset_pair.secondary_currency_id', read_only=True)
-
     class Meta:
         model = Trade
         fields = '__all__'
