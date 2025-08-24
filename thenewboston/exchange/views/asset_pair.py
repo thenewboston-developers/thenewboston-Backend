dad0e02497726ece895ef3cda16adca62911b283
m-naveed-ashraf <mahernaveed531@gmail.com>
2025-08-24 09:04:37 +0500

    paginate asset pairs get endpoint

--- thenewboston/exchange/views/asset_pair.py
+++ thenewboston/exchange/views/asset_pair.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from thenewboston.general.pagination import CustomPageNumberPagination
+
 from ..filters.asset_pair import AssetPairFilter
 from ..models import AssetPair
 from ..serializers.asset_pair import AssetPairSerializer
@@ -8,6 +10,7 @@ from ..serializers.asset_pair import AssetPairSerializer
 
 class AssetPairViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = AssetPairFilter
+    pagination_class = CustomPageNumberPagination
     permission_classes = [IsAuthenticated]
     queryset = AssetPair.objects.all()
     serializer_class = AssetPairSerializer
