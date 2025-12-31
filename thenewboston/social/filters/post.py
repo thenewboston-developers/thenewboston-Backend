6eb1d3637c7f8259318c766b547f6d8bc3e2ccac
Bucky Roberts <buckyroberts@gmail.com>
2025-12-30 20:42:13 -0500

    409 - Profile tab for photos (#410)

--- thenewboston/social/filters/post.py
+++ thenewboston/social/filters/post.py
@@ -5,12 +5,23 @@ from ..models import Post
 
 
 class PostFilter(django_filters.FilterSet):
+    has_image = django_filters.BooleanFilter(method='filter_has_image')
     owner = django_filters.CharFilter(field_name='owner', lookup_expr='exact')
     user = django_filters.CharFilter(method='filter_user')
 
     class Meta:
         model = Post
-        fields = ('owner', 'user')
+        fields = ('has_image', 'owner', 'user')
+
+    @staticmethod
+    def filter_has_image(queryset, name, value):
+        if value is None:
+            return queryset
+
+        if value:
+            return queryset.exclude(image='').exclude(image__isnull=True)
+
+        return queryset.filter(models.Q(image='') | models.Q(image__isnull=True))
 
     @staticmethod
     def filter_user(queryset, name, value):
