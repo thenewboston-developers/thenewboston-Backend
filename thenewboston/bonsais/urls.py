11c5a2a7844e53dedc4bd7bfedb51fe4a0a7a060
Bucky Roberts <buckyroberts@gmail.com>
2026-01-28 22:43:13 -0500

    431 - Remove bonsai (#432)

--- thenewboston/bonsais/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from .views import BonsaiViewSet
-
-router = SimpleRouter(trailing_slash=False)
-router.register('bonsais', BonsaiViewSet)
-
-urlpatterns = router.urls
